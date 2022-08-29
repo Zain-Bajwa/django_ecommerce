deb71e9d4868fc5820b7884b6f27529e4d89abed
Zain-Bajwa <i191288@nu.edu.pk>
2022-08-29 19:27:10 +0500

    Rebase productapi from main

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,26 +9,64 @@ views to create, refresh and verify the tokens.
 
 from django.urls import path
 from rest_framework import routers
-from rest_framework_simplejwt.views import (TokenRefreshView, TokenVerifyView)
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from .views import (
+    AddToCartView,
+    AllProductViewSet,
+    AllUserViewSet,
+    AllCategoryViewSet,
+    CartView,
+    CategoryProductView,
+    CategoryViewSet,
+    ProductViewSet,
+    RemoveFromCartView,
     UserRegisterView,
     UserViewSet,
-    AllUserViewSet,
-    CreateTokenView
+    OrderPlaceView,
+    OrderDetailView,
+    CreateProductView,
+    CreateTokenView,
 )
 
 router = routers.DefaultRouter()
 
 # For retrieve, update, and delete a user
-router.register('user', UserViewSet, basename='user-view')
+router.register("user", UserViewSet, basename="user-view")
 # For retrieve all the users
-router.register('users', AllUserViewSet, basename='user-view')
+router.register("users", AllUserViewSet, basename="user-view")
+
+# For retrieve, update, and delete a Category
+router.register("category", CategoryViewSet, basename="category-view")
+# For retrieve all the categories
+router.register("categories", AllCategoryViewSet, basename="categories-view")
+
+# For retrieve, update, and delete a Product
+router.register('product', ProductViewSet, basename='product-view')
+# For retrieve all the products
+router.register("products", AllProductViewSet, basename="products-view")
+# For create a new product
+# router.register('product/create', CreateProductView,
+#             basename='product-create')
 
 urlpatterns = [
-    path('register/user/', UserRegisterView.as_view(), name='register-user'),
-    path('token/create/', CreateTokenView.as_view(), name='create-token'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='refresh-token'),
-    path('token/verify/', TokenVerifyView.as_view(), name='verify-token'),
+    path("register/user/", UserRegisterView.as_view(), name="register-user"),
+    path("token/create/", CreateTokenView.as_view(), name="create-token"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
+    path("token/verify/", TokenVerifyView.as_view(), name="verify-token"),
+    path(
+        "products/<str:gategory_name>/",
+        CategoryProductView.as_view(),
+        name="category-products-view",
+    ),
+    path("cart/add/", AddToCartView.as_view(), name="add-to-cart"),
+    path("cart/view/<int:user_id>/", CartView.as_view(), name="cart-view"),
+    path("cart/remove", RemoveFromCartView.as_view(), name="remove-from-cart"),
+    path("order/place", OrderPlaceView.as_view(), name="place-order-view"),
+    path("order/detail", OrderDetailView.as_view(), name="order-detail-view"),
+    path("product/create", CreateProductView.as_view(), name="create-product"),
 ]
 
 urlpatterns += router.urls
